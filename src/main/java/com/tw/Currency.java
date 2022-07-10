af25c76fdd2790283169be13e2b101ea1617cffd
Gangothri <gangothrigadige985@gmail.com>
2022-07-10 22:41:27 +0530

    #Requirement1 added a method which checks the equality between one dollar and seventy four rupees eighty five paisa

--- src/main/java/com/tw/Currency.java
+++ src/main/java/com/tw/Currency.java
@@ -1,4 +1,51 @@
 package com.tw;
 
+import java.util.Objects;
+
 public class Currency {
+    private final double value;
+    private final CurrencyType currencyType;
+
+
+    public Currency(double value, CurrencyType currencyType) {
+        this.value = value;
+        this.currencyType = currencyType;
+    }
+
+    private static class CurrencyType {
+        double baseValue;
+
+        public CurrencyType(double baseValue) {
+            this.baseValue = baseValue;
+        }
+
+        final static CurrencyType rupee = new CurrencyType(74.85);
+        final static CurrencyType dollar = new CurrencyType(1);
+
+    }
+
+    public static Currency rupee(double value) {
+        return new Currency(value, CurrencyType.rupee);
+    }
+
+    public static Currency dollar(double value) {
+        return new Currency(value, CurrencyType.dollar);
+    }
+
+    public double convertToRupees() {
+        return value * (CurrencyType.rupee.baseValue / currencyType.baseValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency that = (Currency) o;
+        return this.convertToRupees() == that.convertToRupees();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
--- src/test/java/com/tw/CurrencyTest.java
+++ src/test/java/com/tw/CurrencyTest.java
@@ -1,4 +1,21 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static com.tw.Currency.dollar;
+import static com.tw.Currency.rupee;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
 public class CurrencyTest {
+    @Test
+    void shouldCheckEqualityWhenSeventyFourRupeesEightyFivePaisaAreComparedWithOneDollar() {
+        Currency seventyFourRupeesEightyFivePaisa = rupee(74.85);
+        Currency oneDollar = dollar(1);
+
+        assertThat(seventyFourRupeesEightyFivePaisa, is(equalTo(oneDollar)));
+    }
+
 }
